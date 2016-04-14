30f1c360279837a5ea3fe8368e1473282becc1dd
Michael Vitz <michael.vitz@innoq.com>
2016-04-14 13:29:53 +0200

    Migrate on startup

--- src/simple_calendar/server.clj
+++ src/simple_calendar/server.clj
@@ -1,13 +1,15 @@
 (ns simple-calendar.server
-  (:require  [simple-calendar.core :as core]
-             [environ.core :refer [env]]
-             [ring.adapter.jetty :refer [run-jetty]])
+  (:require [db.migrate :as db]
+            [environ.core :refer [env]]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [simple-calendar.core :as core])
   (:gen-class))
 
 (defn- port  []
   (Integer/parseInt  (or  (env :port) "3002")))
 
 (defn -main  [& args]
+  (db/migrate)
   (core/init)
   (run-jetty
     core/webapp
